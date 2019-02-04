1e7d1c330528c331cba31a3f47a3e365896eeeab
Clarence Mills <clarence.mills@gmail.com>
2019-02-03 19:08:42 -0500

    add option for user to enter URL which gets passed to generate_qr function

--- gen_gr_code.py
+++ gen_gr_code.py
@@ -2,13 +2,12 @@
 
 import pyqrcode
 
-def generate_qr():
-    link_to_post = "https://www.opensitesolutions.com"
-    url = pyqrcode.create(link_to_post)
+def generate_qr(user_url):
+    url = pyqrcode.create(user_url)
     url.png('url.png', scale=8) # Change scale level in increase or decrease size of png image
-    print("Printing QR code")
+    print("Printing QR code for URL " + str(user_url) )
     print(url.terminal())
 
-
 if __name__ == '__main__':
-    generate_qr()
+    user_url = input("Enter URL: ")
+    generate_qr(user_url)
